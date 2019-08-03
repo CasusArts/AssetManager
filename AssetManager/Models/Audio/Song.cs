fe8e4a94e5e8aa9c680303e577005d7257ff5cc6
Fiodor Gorobet <casusarts@gmail.com>
2019-08-03 20:52:15 +0200

    Add Sqlite database, Song Model

---
+++ AssetManager/Models/Audio/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManager.Models.Audio
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Titel { get; set; }
+
+        public string Album { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public string Genre { get; set; }
+
+        public TimeSpan Duration { get; set; }
+    }
+}
